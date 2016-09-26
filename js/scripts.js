3e42fa49cc740172bfb02890ced45d70f8a54597
Joseph Wells <josephwells@Josephs-MacBook-Pro.local>
2016-09-26 01:51:12 -0700

    Updated some javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+
+/*
 interact('.draggable').
   draggable({
     inertia: true,
     autoScroll: true,
+  });*/
+
+//resize script
+interact('.resizable')
+  .resizable({
+    edges: { right: true, bottom: true }
+  })
+  .on('resizemove', function (event) {
+    var target = event.target;
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
   });
